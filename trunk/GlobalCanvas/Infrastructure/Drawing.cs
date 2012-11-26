429b44ea3e1c6aef48406edcb23002825030a45c
MyFake42@gmail.com <MyFake42@gmail.com@84f0a005-be41-ae8b-6085-d6d890eee000>
2012-11-26 17:23:24 +0000

    Added Backbone view and Backbone model

--- trunk/GlobalCanvas/Infrastructure/Drawing.cs
+++ trunk/GlobalCanvas/Infrastructure/Drawing.cs
@@ -2,12 +2,23 @@
 {
     public class Drawing
     {
-        public double FromXPoint { get; set; }
+        //public double FromXPoint { get; set; }
 
-        public double FromYPoint { get; set; }
+        //public double FromYPoint { get; set; }
 
-        public double ToXPoint { get; set; }
+        //public double ToXPoint { get; set; }
 
-        public double ToYPoint { get; set; }
+        //public double ToYPoint { get; set; }
+
+        public Point FromPoint { get; set; }
+
+        public Point ToPoint { get; set; }
+    }
+
+    public struct Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
     }
 }
---
+++ trunk/GlobalCanvas/Scripts/canvas/canvasView.js
@@ -0,0 +1,79 @@
+﻿var CanvasView = Backbone.View.extend({
+    el: $('.main-content'),
+    canvas: document.getElementById('canvas'),
+    canvasContext: null,
+    mouseIsPressed: false,
+    currentModel: new CanvasModel({}),
+    connection: $.connection.canvas,
+        initialize: function () {
+        var view = this;
+        $(view.el).off();
+        _.bindAll(view);
+        this.currentModel.view = this;
+        if (view.canvas.getContext) {
+            view.canvasContext = view.canvas.getContext('2d');
+        }
+        $.connection.hub.start();
+        $.extend(view.connection.client, {
+            drawLine: function (drawing) {
+                view.canvasContext.beginPath();
+                view.canvasContext.lineWidth = 4;
+                view.canvasContext.lineJoin = 'round';
+                view.canvasContext.moveTo(drawing.FromPoint.X, drawing.FromPoint.Y);
+                view.canvasContext.lineTo(drawing.ToPoint.X, drawing.ToPoint.Y);
+                view.canvasContext.stroke();
+            },
+        });
+    },
+    events:
+        {
+            'mousedown #canvas': 'beginDrawing',
+            'mouseup #canvas': 'endDrawing',
+            'mousemove #canvas': 'performMouseMove'
+        },
+    beginDrawing: function (e) {
+        var view = this;
+        view.mouseIsPressed = true;
+        view.currentModel.set({
+            oldPositionX: e.clientX - view.canvas.offsetLeft,
+            oldPositionY: e.clientY - view.canvas.offsetTop - 10
+        });
+    },
+    endDrawing: function () {
+        var view = this;
+        view.mouseIsPressed = false;
+    },
+    performMouseMove: function(e) {
+        var view = this;
+        if (view.mouseIsPressed) {
+            var oldX = view.currentModel.get('oldPositionX'),
+                oldY = view.currentModel.get('oldPositionY'),
+                newX = e.clientX - view.canvas.offsetLeft,
+                newY = e.clientY - view.canvas.offsetTop - 10;
+
+            view.currentModel.set({
+                newPositionX: newX,
+                newPositionY: newY
+            });
+        
+            if (oldX && oldY) {
+                view.canvasContext.beginPath();
+                view.canvasContext.lineWidth = 4;
+                view.canvasContext.lineJoin = 'round';
+                view.canvasContext.moveTo(oldX, oldY);
+                view.canvasContext.lineTo(newX, newY);
+                view.canvasContext.stroke();
+
+                view.connection.server.drawLine(
+                    oldX, oldY,
+                    newX, newY
+                );
+
+                view.currentModel.set({
+                    oldPositionX: newX,
+                    oldPositionY: newY
+                });
+            }
+        }
+    }
+});
